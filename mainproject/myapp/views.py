1447bf78d0decc90b73ff2ee520d75dadf343fc1
pranavpk672 <121079641+pranavpk672@users.noreply.github.com>
2023-11-08 07:16:06 +0530

    categorywise product listing completed

--- mainproject/myapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-#from django.views.decorators.cache import never_cache
-
-# Create your views here.
-#@never_cache
-def index(request):
-    return render(request,'index.html')
